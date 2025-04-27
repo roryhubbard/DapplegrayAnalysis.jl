ebed9c7869fcdcb7acce19bcafdce8594bb171d8
Rory Hubbard <calvin.hubbard@shield.ai>
2025-04-27 14:24:36 -0700

    visualize_swingup

--- src/DapplegrayTest.jl
+++ src/DapplegrayTest.jl
@@ -1,46 +1,71 @@
 module DapplegrayTest
 
-using Colors: RGBA, RGB, @colorant_str
-using StaticArrays: @SVector
-using Rotations: QuatRotation, expm, RotX
-using CoordinateTransformations: LinearMap
-using GeometryBasics: HyperRectangle, Mesh, Point, TriangleFace, Vec, Point3d,
-                      Cylinder, HyperSphere
-using MeshCat: Visualizer, setobject!, MeshPhongMaterial, setanimation!,
-               settransform!, Animation, atframe
+using Colors
+using CoordinateTransformations
+using DapplegrayDynamics
+using GeometryBasics
+using MeshCat
+using RobotDynamics: AbstractModel, RBState, AbstractTrajectory, states
+using RobotZoo
+using Rotations
+using StaticArrays
+using TrajectoryOptimization
 
-using DapplegrayDynamics: pendulum, simulate_pendulum
+export visualize_swingup
 
-export test_simulate
+"""
+Animate the trajectory of a rigid body in MeshCat
+    Assumes the robot geometry is already loaded into `vis["robot"]`
+"""
+visualize!(vis, model::AbstractModel, Z::AbstractTrajectory) =
+    visualize!(vis, model, Z[end].t, states(Z))
+
+"""
+    visualize!(vis, model, tf, Xs...; kwargs...)
+
+Visualize many different trajectories of the same model.
+
+# Arguments
+* `probs`: any struct that supports `TO.get_model` and `TO.get_trajectory`
+* `Zs`: any `AbstractTrajectory`
+* `Xs`: a vector of state vectors
+"""
+function visualize!(vis, model::AbstractModel, tf::Real, Xs::Vector)
+    N = length(Xs)
+    fps = Int(floor((N-1)/tf))
+    anim = MeshCat.Animation(vis, fps=fps)
+    for k = 1:N
+        atframe(anim, k) do
+            robot = vis
+            visualize!(robot, model, Xs[k])
+        end
+    end
+    setanimation!(vis, anim)
+end
+
+function visualize!(vis, model::RobotZoo.Pendulum, x::StaticVector)
+    θ = x[1]
+    q = expm((pi-θ) * @SVector [1,0,0])
+    settransform!(vis["geom"], LinearMap(QuatRotation(q)))
+end
 
 # Pendulum
-function _set_mesh!(vis, length)
-    hinge = Cylinder(Point3d(0.05, 0, 0), Point3d(-0.05, 0, 0), 0.05)
-    rod   = Cylinder(Point3d(0, 0, 0), Point3d(0, 0, length), 0.01)
-    mass  = HyperSphere(Point3d(0, 0, length), 0.05)
+function _set_mesh!(vis, model::RobotZoo.Pendulum; length=model.len)
+    hinge = Cylinder(Point3d(0.05,0,0), Point3d(-0.05,0,0), 0.05)
+    rod   = Cylinder(Point3d(0,0,0), Point3d(0,0,length), 0.01)
+    mass  = HyperSphere(Point3d(0,0,length), 0.05)
     setobject!(vis["geom"]["hinge"], hinge, MeshPhongMaterial(color=colorant"black"))
     setobject!(vis["geom"]["rod"  ], rod,   MeshPhongMaterial(color=colorant"blue"))
     setobject!(vis["geom"]["mass" ], mass , MeshPhongMaterial(color=colorant"red"))
 end
 
-function test_simulate()
+function visualize_swingup()
     vis = Visualizer()
     open(vis)
-    _set_mesh!(vis, pendulum.len)
-
-    sol = simulate_pendulum()
-
-    fps = 1000
-    anim = Animation(vis, fps=fps)
-    for (tt, uu) in zip(sol.t, sol.u)
-        frame = round(Int, tt * fps)
-        atframe(anim, frame) do
-            q = expm((π - uu[1]) * @SVector [1,0,0])
-            settransform!(vis["geom"], LinearMap(QuatRotation(q)))
-        end
-    end
-
-    setanimation!(vis, anim)
+    model = RobotZoo.Pendulum()
+    _set_mesh!(vis, model)
+    solver  = swingup()
+    visualize!(vis, model, get_trajectory(solver))
 end
 
 end
