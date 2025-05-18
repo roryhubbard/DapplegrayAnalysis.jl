78e35340502b5069dde70ae13b4fec2dbbaa843d
Rory Hubbard <calvin3h@gmail.com>
2025-05-18 12:47:22 -0700

    doublependulum with RididBodyDyanmics output

--- src/DapplegrayAnalysis.jl
+++ src/DapplegrayAnalysis.jl
@@ -5,67 +5,62 @@ using CoordinateTransformations
 using DapplegrayDynamics
 using GeometryBasics
 using MeshCat
-using RobotDynamics: AbstractModel, RBState, AbstractTrajectory, states
-using RobotZoo
 using Rotations
 using StaticArrays
-using TrajectoryOptimization
 
-export visualize_swingup
+export visualize_doublependulum
 
-"""
-Animate the trajectory of a rigid body in MeshCat
-    Assumes the robot geometry is already loaded into `vis["robot"]`
-"""
-visualize!(vis, model::AbstractModel, Z::AbstractTrajectory) =
-    visualize!(vis, model, Z[end].t, states(Z))
-
-"""
-    visualize!(vis, model, tf, Xs...; kwargs...)
-
-Visualize many different trajectories of the same model.
-
-# Arguments
-* `probs`: any struct that supports `TO.get_model` and `TO.get_trajectory`
-* `Zs`: any `AbstractTrajectory`
-* `Xs`: a vector of state vectors
-"""
-function visualize!(vis, model::AbstractModel, tf::Real, Xs::Vector)
-    N = length(Xs)
-    fps = Int(floor((N-1)/tf))
+function visualize!(vis, model::Symbol, ts::Vector, qs::Vector, args...; kwargs...)
+    N = length(ts)
+    fps = Int(floor((N-1)/(ts[end] - ts[1])))
     anim = MeshCat.Animation(vis, fps=fps)
     for k = 1:N
         atframe(anim, k) do
             robot = vis
-            visualize!(robot, model, Xs[k])
+            visualize!(robot, Val(model), qs[k], args...; kwargs...)
         end
     end
     setanimation!(vis, anim)
 end
 
-function visualize!(vis, model::RobotZoo.Pendulum, x::StaticVector)
-    θ = x[1]
-    q = expm((pi-θ) * @SVector [1,0,0])
-    settransform!(vis["geom"], LinearMap(QuatRotation(q)))
+function visualize!(vis, ::Val{:doublependulum}, x, link1length::Real)
+    e1 = @SVector [1,0,0]
+    q1,q2 = expm((x[1]+π)*e1), expm(x[2]*e1)
+    settransform!(vis["shoulder"], LinearMap(UnitQuaternion(q1)))
+    settransform!(vis["shoulder", "upperlink", "elbow"], LinearMap(UnitQuaternion(q2)))
 end
 
-# Pendulum
-function _set_mesh!(vis, model::RobotZoo.Pendulum; length=model.len)
-    hinge = Cylinder(Point3d(0.05,0,0), Point3d(-0.05,0,0), 0.05)
-    rod   = Cylinder(Point3d(0,0,0), Point3d(0,0,length), 0.01)
-    mass  = HyperSphere(Point3d(0,0,length), 0.05)
-    setobject!(vis["geom"]["hinge"], hinge, MeshPhongMaterial(color=colorant"black"))
-    setobject!(vis["geom"]["rod"  ], rod,   MeshPhongMaterial(color=colorant"blue"))
-    setobject!(vis["geom"]["mass" ], mass , MeshPhongMaterial(color=colorant"red"))
+function set_mesh!(vis, model::Symbol, args...; kwargs...)
+    set_mesh!(vis, Val(model), args...; kwargs...)
 end
 
-function visualize_swingup()
+function set_mesh!(vis, ::Val{:doublependulum}, link1length, link2length; color=colorant"blue", thick=0.05)
+    hinge = Cylinder(Point3d(-thick/2.0,0,0), Point3d(thick/2.0,0,0), thick)
+    dim1  = Vec(thick, thick, link1length)
+    upperlink = Rect3d(Vec(-thick/2,-thick/2,0),dim1)
+    dim2  = Vec(thick, thick, link2length)
+    lowerlink = Rect3d(Vec(-thick/2,-thick/2,0),dim2)
+    mat1 = MeshPhongMaterial(color=colorant"grey")
+    mat2 = MeshPhongMaterial(color=color)
+    setobject!(vis["shoulder"], hinge, mat1)
+    setobject!(vis["shoulder", "upperlink"], upperlink, mat2)
+    setobject!(vis["shoulder", "upperlink","elbow"], hinge, mat1)
+    setobject!(vis["shoulder", "upperlink","elbow","lowerlink"], lowerlink, mat2)
+    settransform!(vis["upperlink"], Translation(0,0,-link1length))
+    e1 = @SVector [1,0,0]
+    q1 = expm(π*e1)
+    settransform!(vis["shoulder"], LinearMap(UnitQuaternion(q1)))
+    settransform!(vis["shoulder", "upperlink","elbow"], Translation(0,0,link1length))
+end
+
+function visualize_doublependulum()
     vis = Visualizer()
+    link1length = 1.0
+    link2length = 1.0
+    set_mesh!(vis, :doublependulum, link1length, link2length)
+    ts, qs, vs = doublependulum()
+    visualize!(vis, :doublependulum, ts, qs, link1length)
     open(vis)
-    model = RobotZoo.Pendulum()
-    _set_mesh!(vis, model)
-    solver  = swingup()
-    visualize!(vis, model, get_trajectory(solver))
 end
 
 end
